3ec7e41c0093b440d86bd4700070d03d9b57e5d2
Mitchell Daddario <mitchelldaddario@gmail.com>
2024-01-31 19:05:48 -0600

    started project

---
+++ app/src/main/java/com/example/lesson3/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.lesson3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Navigate to the LoginActivity page when the login button is clicked.
+        val loginButton = findViewById<Button>(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
